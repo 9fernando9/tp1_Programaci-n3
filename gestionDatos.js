093c105ce3718bd4a88dc1045a4a6975a292b474
9fernando9 <36698254+9fernando9@users.noreply.github.com>
2025-08-15 09:35:26 -0400

    gestionDatos.js
    Código acomodado para su mejor lectura y compresión

--- gestionDatos.js
+++ gestionDatos.js
@@ -14,98 +14,113 @@ let productos = [
 ];
 
 //MOSTRAR PRODUCTOS
-console.log("Listado de productos:");
-console.table(productos);
-console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
+function mostrarProductos(){
+  console.log("Listado de productos:");
+  console.table(productos);
+}
 //PUNTO 2
-console.log(`\nOperaciones Basicas:`); 
-//MUESTRA CANTIDAD DE PRODUCTOS
-console.log(`2.1 - Cantidad de productos: ${productos.length}`);
-console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
-//IMPRIMER SEGUNDO Y CUARTO PRODUCTO
-console.log(`2.2 - Segundo producto: ${productos[1].nombre}`);
-console.log(`2.2 - Cuarto producto: ${productos[3].nombre}`);
-console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
+function mostrarCantidadProductos() {
+  console.log(`2.1 - Cantidad de productos: ${productos.length}`);
+}
+function mostrarSegundoYCuartoProducto() {
+  console.log(`2.2 - Segundo producto: ${productos[1].nombre}`);
+  console.log(`2.2 - Cuarto producto: ${productos[3].nombre}`);
+}
 //PUNTO 3
-console.log('\x1b[31m%s\x1b[0m',`\n===================================`);
-console.log(`3. Recorrido del Array`); 
-console.log(`3.1 Recorrer array utilizando for .. of`); 
-for (let producto of productos) {
-  console.log(`Producto: ${producto.nombre},  Precio: $${producto.precio}`);
+function recorrerForOf() {
+  console.log(`3. Recorrido del Array utilizando for .. of`);
+  for (let producto of productos) {
+    console.log(`Producto: ${producto.nombre},  Precio: $${producto.precio}`);
+  }
 }
-console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
-console.log(`\n3.1 Recorrer array utilizando forEach`); 
-productos.forEach(function(producto) {
-  console.log(`Producto: ${producto.nombre}, Precio: $${producto.precio}`);
-});
-console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
+function recorrerForEach() {
+  console.log(`3.1 Recorrer array utilizando forEach`);
+  productos.forEach(function(producto) {
+    console.log(`Producto: ${producto.nombre}, Precio: $${producto.precio}`);
+  });
+}
 //PUNTO 4
-console.log('\x1b[31m%s\x1b[0m',`\n===================================`); 
-console.log(`4. Manipulación de Arrays`);
-console.log(`\n4.1 Agregar dos elementos con push()`);
-let nuevoProducto1 = { id: 6, nombre: `Impresora`, precio: 30000, stock: 10 };
-console.log("Nuevo producto agregado: ",nuevoProducto1);
-let nuevoProducto2 = { id: 7, nombre: `Scaner`, precio: 20000, stock: 8 };
-console.log("Nuevo producto agregado: ",nuevoProducto2);
-productos.push(nuevoProducto1, nuevoProducto2);
-//MOSTRAR PRODUCTOS
-console.log(`Listado de productos con elementos agregados:`);
-console.table(productos);
-console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
-console.log(`\n4.2 Eliminar el último elemento con pop()`);
-const productoEliminado = productos.pop();
-console.log(`Producto eliminado:${productoEliminado.nombre}`);
-console.log(`Listado de productos resultantes`);
-console.table(productos);
-console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
-//unshift().
-console.log(`\n4.3 Agregar un nuevo elemento al inicio del array productos utilizando unshift()`);
-let nuevoProducto3 = { id: 8, nombre: `Parlantes`, precio: 22000, stock: 15 };
-console.log("Nuevo producto agregado:",nuevoProducto3);
-productos.unshift(nuevoProducto3);
-console.log(`Listado de productos con elemento agregado al inicio:`);
-console.table(productos);
-console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
-//shift().
-console.log(`\n4.4 Eliminar el primer elemento del array productos utilizando shift()`);
-const ProductoEliminado2 = productos.shift();
-console.log(`Producto eliminado: ${ProductoEliminado2.nombre}`);
-console.log(`Listado de productos resultantes`);
-console.table(productos);
-console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
-//filter().
-let productosConStock = productos.filter(producto => producto.stock > 0);
-console.log(`\n4.5 Productos con stock mayor a 0:`);
-console.table(productosConStock);
-console.log("Lista de productos original:");
-console.table(productos);
-console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
-/**6.  Crear   un   nuevo   array   llamado  nombresProductos  que   contenga   solo   los   nombres   de   todos   los 
-productos en el inventario utilizando map().**/
-let nombresProductos = productos.map(producto => producto.nombre);
-console.log("\n4.6 Array de nombres de productos:");
-console.table(nombresProductos);
-console.log("Lista de productos original:");
-console.table(productos);
-console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
-/**7. Encontrar y guardar en una variable el primer producto en productos que tenga un id específico (ej. id:3) 
-utilizando find(). Si no lo encuentra, indicar que no existe.**/
-console.log("\n4.7 Buscando producto con id específico: 3");
-let idBuscado = 3;
-let productoEncontrado = productos.find(producto => producto.id === idBuscado);
-if (productoEncontrado) {
-  console.log(`Producto encontrado con id ${idBuscado}:`, productoEncontrado);
-} else {
-    console.log(`No existe un producto con id ${idBuscado}.`);
+function agregarDosElementosConPush() {
+  console.log(`4.1 Agregar dos elementos con push()`);
+  let nuevoProducto1 = { id: 6, nombre: `Impresora`, precio: 30000, stock: 10 };
+  console.log("Nuevo producto agregado: ",nuevoProducto1);
+  let nuevoProducto2 = { id: 7, nombre: `Scaner`, precio: 20000, stock: 8 };
+  console.log("Nuevo producto agregado: ",nuevoProducto2);
+  productos.push(nuevoProducto1, nuevoProducto2);
+  mostrarProductos();
 }
-console.log("Lista de productos original:");
-console.table(productos);
+function eliminarUltimoProductoConPop() {
+  console.log(`4.2 Eliminar el último elemento con pop()`);
+  const productoEliminado = productos.pop();
+  console.log(`Producto eliminado: ${productoEliminado.nombre}`);
+  mostrarProductos();
+}
+function agregarElementoAlInicioConUnshif() {
+  console.log(`4.3 Agregar un nuevo elemento al inicio del array productos utilizando unshift()`);
+  let nuevoProducto3 = { id: 8, nombre: `Parlantes`, precio: 22000, stock: 15 };
+  console.log("Nuevo producto agregado:",nuevoProducto3);
+  productos.unshift(nuevoProducto3);
+  mostrarProductos();
+}
+function eliminarPrimerProductoConShift() {
+  console.log(`4.4 Eliminar el primer elemento del array productos utilizando shift()`);
+  const ProductoEliminado2 = productos.shift();
+  console.log(`Producto eliminado: ${ProductoEliminado2.nombre}`);
+  mostrarProductos();
+}
+function mostrarProductosFiltradosConFilter() {
+  let productosConStock = productos.filter(producto => producto.stock > 0); 
+  console.log(`4.5 Productos con stock mayor a 0:`);
+  console.table(productosConStock);
+  mostrarProductos();
+}
+function crearNuevoArrayConMap() {
+  let nombresProductos = productos.map(producto => producto.nombre);
+  console.log("4.6 Array de nombres de productos:");
+  console.table(nombresProductos);
+  mostrarProductos();
+}
+function buscarProductoPorId(id) {
+  let productoEncontrado = productos.find(producto => producto.id === id);
+  if (productoEncontrado) {
+    console.log(`Producto encontrado con id ${id}:`, productoEncontrado);
+  } else {
+    console.log(`No existe un producto con id ${id}.`);
+  }
+  mostrarProductos();
+}
+function ordenarProductosPorPrecioConShort() {
+  let productosOrdenados = [...productos].sort((a, b) => b.precio - a.precio);
+  console.log("4.8 Productos ordenados por precio (decreciente):");
+  console.table(productosOrdenados);
+  mostrarProductos();
+}
+
+
+mostrarProductos();
 console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
-/**8. Crear un nuevo array llamado  productosOrdenados que contenga los productos ordenados por precio 
-en orden decreciente. (investigar método sort()).**/
-let productosOrdenados = [...productos].sort((a, b) => b.precio - a.precio);
-console.log("\n4.8 Productos ordenados por precio (decreciente):");
-console.table(productos);
-console.log("Lista de productos original:");
-console.table(productos);
+mostrarCantidadProductos();
+console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
+mostrarSegundoYCuartoProducto();
+console.log('\x1b[31m%s\x1b[0m',`\n===================================`);
+recorrerForOf();
+console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
+recorrerForEach();
+console.log('\x1b[31m%s\x1b[0m',`\n===================================`); 
+agregarDosElementosConPush();
+console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
+eliminarUltimoProductoConPop();
+console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
+agregarElementoAlInicioConUnshif();
+console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
+eliminarPrimerProductoConShift();
+console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
+mostrarProductosFiltradosConFilter();
+console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
+crearNuevoArrayConMap();
+console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
+console.log("\n4.7 Buscando producto con id específico: 3");
+buscarProductoPorId(3);
+console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
+ordenarProductosPorPrecioConShort();
 console.log('\x1b[32m%s\x1b[0m',"-------------------------------------------------");
